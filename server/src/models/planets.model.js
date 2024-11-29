874e4ede9fb5150a211d363691236fa23a31297d
Your Name <michaelalex03@outlook.com>
2024-11-29 13:48:39 -0600

    added scripts and setup file processing to recieve planets for get request

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,44 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = planets;
+
+const results = []; //array of habitable planets
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+//We return a promise to know when the results array has been populated so that module.exports doesnt export results when it is not finished populating
+function loadPlanetsData() {
+    //pipe() readable steam pipes to writable stream
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    results.push(data);
+                }
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err);
+            })
+            .on('end', () => {
+                console.log('testtt');
+                resolve(); //resolve when all data has been streamed in and parsed
+            })
+    });
+}
+
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: results,
+};
