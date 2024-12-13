e4f852e900122c7f7c24a77c5b766e11bca780bf
Your Name <michaelalex03@outlook.com>
2024-12-12 23:03:47 -0600

    made abort function work with monogDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,15 +20,21 @@ const launch = {
 saveLaunch(launch);
 
 
-function existsLaunchWithId(launchId){
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId){
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
 
-function abortLaunchById(launchId){
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted
+async function abortLaunchById(launchId){
+    const aborted =  await launchesDatabase.updateOne({
+        flightNumber: launchId,
+    }, {
+        upcoming: false,
+        success: false,
+    });
+
+    return aborted.modifiedCount === 1;
 }
 
 async function getLatestFlightNumber(){
