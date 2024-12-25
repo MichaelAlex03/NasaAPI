29193ee47bbfe76951e593124dfee34de8b4601d
Your Name <michaelalex03@outlook.com>
2024-12-25 12:47:33 -0600

    adding CI/CD pipeline

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,9 +3,6 @@ const axios = require('axios');
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
-const launches = new Map();
-
-
 const DEFAULT_FLIGHT_NUMBER = 100;
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
